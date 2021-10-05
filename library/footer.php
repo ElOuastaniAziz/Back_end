49c2ee3b78413a0d39405ac4784f4eb81e4e0bf9
ElOuastaniAziz <designsyaz@gmail.com>
2021-10-06 00:42:09 +0200

    deleted

--- library/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-            <p>By Abdelaziz <?php echo date("Y");?> </p>
-            <a href="weather.php" style="margin-left:20px;">
-            <img src="images/weather.png" alt="" style="width:35px; margin-top:-20px;"/></a>
-           
-</footer>
